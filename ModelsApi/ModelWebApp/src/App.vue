a33c826dbbc97f26aa94ca9c9f731f1dfe03b33b
Jonathan Hartvigsen Juncker <au618464@uni.au.dk>
2020-05-15 15:34:36 +0200

    Login stuff

--- ModelsApi/ModelWebApp/src/App.vue
+++ ModelsApi/ModelWebApp/src/App.vue
@@ -3,11 +3,13 @@
         <nav>
             <router-link to="/">Home</router-link> |
             <router-link to="/login">login</router-link> |
-            <router-link to="/CreateNewModel">CreateNewModel</router-link>
-            <router-link to="/CreateNewManager">CreateNewManager</router-link>
-            <router-link to="/CreateNewJob">CreateNewJob</router-link>
-            <router-link to="/JobView">JobView</router-link>
+            <router-link to="/CreateNewModel">CreateNewModel</router-link> |
+            <router-link to="/CreateNewManager">CreateNewManager</router-link> |
+            <router-link to="/CreateNewJob">CreateNewJob</router-link> |
+            <router-link to="/JobView">JobView</router-link> |
         </nav>
+
+   
         <!-- component matched by the route will render here -->
         <router-view></router-view>>
     </div>
--- ModelsApi/ModelWebApp/src/components/login.vue
+++ ModelsApi/ModelWebApp/src/components/login.vue
@@ -1,26 +1,48 @@
 <template>
-    <div class="Login">
-
-        <md-card>
-
-            <md-card-header>
-                <div><h>Login</h></div>
-            </md-card-header>
-
-            <md-button>Login</md-button>
-        </md-card>
+    <div class="md-layout-item">
+        
+            <md-card class="md-primary">
+                <md-ripple>
+                    <md-card-header>
+                        <div class="md-title">Login</div>
+                    </md-card-header>
+                    <md-content>
+                        <md-field md-clearable>
+                            <label>UserName</label>
+                            <md-input> v-model="UserName"</md-input>
+                        </md-field>
+                        <md-field>
+                            <label>Password</label>
+                            <md-input> v-model="Password"</md-input>
+                        </md-field>
+                    </md-content>
+                    <md-card-action>
+                        <md-button type="submit" class="md-raised">Login</md-button>
+                    </md-card-action>
+                </md-ripple>
+            </md-card>
+        
     </div>
 </template>
 
 <script>
     export default {
         name: 'login',
-        props: {
-            msg: String
-        }
+        data: () => ({
+            UserName: 'UserName',
+            PassWord: 'Password'
+        })
     };
 </script>
 
 <style scoped>
+    
+
+    .md-layout-item 
+    {
+        height: 400px;
+        width: 400px;
+          
+    }
 </style>
 
