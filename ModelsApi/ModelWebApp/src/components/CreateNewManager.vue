dc81740538c64adedf8022736cbea7269f1f74bf
Jonathan Hartvigsen Juncker <au618464@uni.au.dk>
2020-05-16 13:28:27 +0200

    lolll

--- ModelsApi/ModelWebApp/src/components/CreateNewManager.vue
+++ ModelsApi/ModelWebApp/src/components/CreateNewManager.vue
@@ -1,9 +1,34 @@
 <template>
     <div class="CreateNewManager">
-        <form @submit.prevent ="createNewManagerFunction">
-            <md-card class="">
-            </md-card>
-        </form>
+        <form @submit ="createNewManagerFunction">
+            <md-card>
+                    <md-card-header>
+                        <div class="md-title">Create New Manager</div>
+                    </md-card-header>
+                    <md-content>
+                        <md-field>
+                            <label for="firstname">First Name</label>
+                            <md-input type="text" class="form-group"
+                                      v-model="model.firstname" :md-options="countries"
+                                      id="firstname" />
+                        </md-field>
+
+                        <md-field>
+                            <label for="lastname">Last Name</label>
+                            <md-input type="text" class="form-group"
+                                      v-model="model.lastname"
+                                      id="lastname" />
+                        </md-field>
+
+                        <md-field>
+                            <label for="email">Email</label>
+                            <md-input type="text" class="form-group"
+                                      v-model="model.email"
+                                      id="email" />
+                        </md-field>
+                     </md-content>
+           </md-card>
+        </form> 
     </div>
 </template>
 
