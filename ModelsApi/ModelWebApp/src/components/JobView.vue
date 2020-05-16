d40db816beaef715888d7694207437a9a3b26c11
Jonathan Hartvigsen Juncker <au618464@uni.au.dk>
2020-05-16 12:17:32 +0200

    Jobview

--- ModelsApi/ModelWebApp/src/components/JobView.vue
+++ ModelsApi/ModelWebApp/src/components/JobView.vue
@@ -1,7 +1,23 @@
 <template>
-    <div class="JobView">
-        <h1>{{ msg }}</h1>
-        <p>Hej med dig jobview</p>
+    <div>
+        <md-card>
+            <md-card-header>
+             <div class="md-title">Job liste</div>
+             </md-card-header>
+        
+            <md-card-content>
+                <md-table>
+                <md-table-row>
+                    <md-table-head>Customer</md-table-head>
+                    <md-table-head>Start date</md-table-head>
+                    <md-table-head>Number of days</md-table-head>
+                    <md-table-head>location</md-table-head>
+                    <md-table-head>Comment</md-table-head>
+                </md-table-row>
+                </md-table>
+            </md-card-content>
+
+        </md-card>
     </div>
 </template>
 
