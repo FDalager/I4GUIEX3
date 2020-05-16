2bd00d960334ce147a70144367251007e3f583fe
Jonathan Hartvigsen Juncker <au618464@uni.au.dk>
2020-05-16 10:42:24 +0200

    Login

--- ModelsApi/ModelWebApp/src/components/login.vue
+++ ModelsApi/ModelWebApp/src/components/login.vue
@@ -1,24 +1,33 @@
 <template>
     <div class="md-layout-item">
         
-            <md-card class="md-primary">
+            <md-card class="md-layout-item md-size-50 md-small-size-100">
                 <md-ripple>
                     <md-card-header>
                         <div class="md-title">Login</div>
                     </md-card-header>
+
                     <md-content>
-                        <md-field md-clearable>
+                       
+                        <md-field md-clearable class="userNameField">
                             <label>UserName</label>
                             <md-input> v-model="UserName"</md-input>
+                            <md-option value="center">Center</md-option>
                         </md-field>
-                        <md-field>
+
+                        <md-field class="passwordField">
                             <label>Password</label>
                             <md-input> v-model="Password"</md-input>
+                            <md-option value="center">Center</md-option>
                         </md-field>
+                        
                     </md-content>
+
                     <md-card-action>
                         <md-button type="submit" class="md-raised">Login</md-button>
                     </md-card-action>
+
+
                 </md-ripple>
             </md-card>
         
@@ -37,12 +46,22 @@
 
 <style scoped>
     
-
-    .md-layout-item 
+    .passwordField
     {
+        position: relative;
+    }
+
+    .userNameField
+    {
+        position: relative;
+    }
+
+    /*.md-layout-item 
+    {
+        position: relative;
         height: 400px;
         width: 400px;
           
-    }
+    }*/
 </style>
 
